91ce7444088213fbd72b6afd1d447f26f16dd8a9
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-04-12 16:20:40 +0300

    Оформил скачивание фотографий как функцию

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 from pathlib import Path
 
+
+IMG_FOLDER_NAME = 'images'
+
 def download_image(url, path):
     response = requests.get(url)
     response.raise_for_status()
@@ -9,22 +12,25 @@ def download_image(url, path):
         file.write(response.content)
 
 
-def get_latest_launch_images():
+def fetch_spacex_last_launch():
     response = requests.get('https://api.spacexdata.com/v5/launches/')
     response.raise_for_status()
-    return response.json()[12]['links']['flickr']['original']
-
+    images = response.json()[12]['links']['flickr']['original']
+    for img_number, img_url in enumerate(images):
+        download_image(img_url, Path(IMG_FOLDER_NAME) / (f'spacex{img_number}.jpg'))
+        
+        
 def main():
-    img_folder = 'images'
-    Path(img_folder).mkdir(parents=True, exist_ok=True)
+    
+    Path(IMG_FOLDER_NAME).mkdir(parents=True, exist_ok=True)
     '''
     filename = 'hubble.jpeg'
     file_path = Path(img_folder) / (filename)
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
     download_image(url, file_path)
     '''
-    for img_number, img_url in enumerate(get_latest_launch_images()):
-        download_image(img_url, Path(img_folder) / (f'spacex{img_number}.jpg'))
+    fetch_spacex_last_launch()
+
 
 if __name__ == '__main__':
     main()
