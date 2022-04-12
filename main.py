c59e52156bb671fd25dc808fd627c2ad96a74cde
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-04-12 16:06:01 +0300

    Реализовал получение фото с последнего запуска

--- main.py
+++ main.py
@@ -9,16 +9,23 @@ def download_image(url, path):
         file.write(response.content)
 
 
+def get_latest_launch_images():
+    response = requests.get('https://api.spacexdata.com/v5/launches/')
+    response.raise_for_status()
+    return response.json()[12]['links']['flickr']['original']
+
 def main():
-    filename = 'hubble.jpeg'
+    '''
     img_folder = 'images'
-    
     Path(img_folder).mkdir(parents=True, exist_ok=True)
+    
+    filename = 'hubble.jpeg'
     file_path = Path(img_folder) / (filename)
-    
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    
     download_image(url, file_path)
+    '''
+    for img in get_latest_launch_images():
+        print(img)
 
 if __name__ == '__main__':
     main()
