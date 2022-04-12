a80e50ab7c409724bc331dc366dcbb29f1726fbb
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-04-12 15:02:42 +0300

    Оформил скачивание картинки как функцию

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import requests
 from pathlib import Path
 
+def download_image(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+
 def main():
     filename = 'hubble.jpeg'
     img_folder = 'images'
@@ -10,11 +18,7 @@ def main():
     
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
     
-    response = requests.get(url)
-    response.raise_for_status()
-    
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
+    download_image(url, file_path)
 
 if __name__ == '__main__':
     main()
