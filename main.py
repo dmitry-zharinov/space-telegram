bc4d34e00aacdbb067783043515bc9ffd9f05e00
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-04-12 14:37:07 +0300

    Сделал загрузку картинки в созданную папку

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from pathlib import Path
+
+def main():
+    filename = 'hubble.jpeg'
+    img_folder = 'images'
+    
+    Path(img_folder).mkdir(parents=True, exist_ok=True)
+    file_path = Path(img_folder) / (filename)
+    
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+if __name__ == '__main__':
+    main()
