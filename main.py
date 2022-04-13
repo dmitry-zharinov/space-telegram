e8572e7f27558024f3ecc7541effd9427880e571
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-04-13 19:08:11 +0300

    Реализовал загрузку картинок дня

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import requests
-from pathlib import Path
+import os
 from datetime import datetime
+from pathlib import Path
+from urllib.parse import unquote, urlsplit
+
+import requests
+from dotenv import load_dotenv
 
 IMG_FOLDER_NAME = 'images'
-
+COUNT_APOD_IMAGES = 20
 
 def download_image(url, path):
     response = requests.get(url)
     response.raise_for_status()
-
     with open(path, 'wb') as file:
         file.write(response.content)
+
+
+def get_extension_from_url(url):
+    url_split = urlsplit(url)
+    return os.path.splitext(unquote(url_split.path))[1]
 
 
 def fetch_spacex_last_launch():
@@ -23,8 +31,8 @@ def fetch_spacex_last_launch():
 
 
 def fetch_nasa_epic_photo():
-    nasa_api = 'https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY'
-    response = requests.get(nasa_api)
+    response = requests.get(f'https://api.nasa.gov/EPIC/api/natural/images?'
+                            f'api_key={os.environ["NASA_API_KEY"]}')
     response.raise_for_status()
     for img_number, img in enumerate(response.json()):
         img_date = datetime.fromisoformat(img['date'])
@@ -37,10 +45,25 @@ def fetch_nasa_epic_photo():
                        Path(IMG_FOLDER_NAME) / (f'epic{img_number}.jpg'))
 
 
+def fetch_nasa_APOD():
+    payload = {
+        'count': COUNT_APOD_IMAGES,
+    }
+    response = requests.get(f'https://api.nasa.gov/planetary/apod?'
+                            f'api_key={os.environ["NASA_API_KEY"]}',
+                            params=payload)
+    response.raise_for_status()
+    for img_number, img in enumerate(response.json()):
+        download_image(img['url'],
+                       Path(IMG_FOLDER_NAME) / (f'apod{img_number}.jpg'))
+
+
 def main():
+    load_dotenv()
     Path(IMG_FOLDER_NAME).mkdir(parents=True, exist_ok=True)
     fetch_spacex_last_launch()
     fetch_nasa_epic_photo()
+    fetch_nasa_APOD()
 
 
 if __name__ == '__main__':
